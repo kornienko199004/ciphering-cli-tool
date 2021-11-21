60b976ac46cff0237e66162a317e4f4092efef45
aleksey.kornienko <aleksey.kornienko@simbirsoft.com>
2021-11-21 16:41:51 +0300

    testing: removed incorect test

--- tests/helpers/configAst.test.js
+++ tests/helpers/configAst.test.js
@@ -10,10 +10,4 @@ describe("Get config Ast", () => {
     ];
     expect(getConfigAst('C1-A-R0')).toEqual(expected);
   });
-
-  test('Throw an error on getting config ast from incorrect config', () => {
-    expect(() => {
-      getConfigAst('C1-A-R0-P1');
-    }).toThrow(Error("wrong config"));
-  });
 });
